b3f233132decf0b5f4c509b54839967b07db5f02
Andreas Kocman <e0302840@student.tuwien.ac.at>
2016-01-21 16:24:08 +0100

    testapps finally deleted from webapps folder

--- src/main/webapp/testapps/js/directives.js
+++
@@ -1,6 +0,0 @@
-angular.module('taskit.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
